bf3f5967d9c5f6777d1ee60b8b9809990ccfa4e1
Eh-haque <ehtisamul@gmail.com>
2021-08-21 16:35:14 +0600

    all last

--- script/script.js
+++ script/script.js
@@ -1,115 +1,77 @@
-// ram 
+// ram by id
 const ram8gb = document.getElementById('ram-8gb');
 const ram16gb = document.getElementById('ram-16gb');
-const ramPrice = document.getElementById('ram-price')
+const ramPrice = document.getElementById('ram-price');
 
-// storage
+// storage by id
 const storage256gb = document.getElementById('storage-256gb');
 const storage512gb = document.getElementById('storage-512gb');
 const storage1tb = document.getElementById('storage-1tb');
 const storagePrice = document.getElementById('storage-price')
 
-// delivery
+// delivery by id
 const freeDelivery = document.getElementById('free-delivery');
 const paidDelivery = document.getElementById('paid-delivery');
 const deliveryCost = document.getElementById('delivery-cost');
 
-// coupon
+// coupon by id
 const couponCode = document.getElementById('coupon-code');
-const couponButton = document.getElementById('coupon-button');
 
-// price 
+// price by id
 const bestPrice = document.getElementById('best-Price');
 const total = document.getElementById('total-price');
 const bottomTotal = document.getElementById('bottom-total');
 
-// return is class present
-function isContain (elem) {
-    return elem.classList.contains('selected');
-}
-
-// ram
+// ram price update
 ram8gb.addEventListener('click', function () {
-    if(!isContain(ram8gb)) {
-        ram8gb.classList.add('selected')
-        ram16gb.classList.remove('selected');
-    }
     ramPrice.innerText = '0';
     updateTotal()
 });
 ram16gb.addEventListener('click', function () {
-    if(!isContain(ram16gb)) {
-        ram8gb.classList.remove('selected');
-        ram16gb.classList.add('selected');
-    }
     ramPrice.innerText = '180';
     updateTotal()
 });
 
-// storage
+// storage price update
 storage256gb.addEventListener('click', function () {
-    if(!isContain(storage256gb)) {
-        storage256gb.classList.add('selected');
-        storage512gb.classList.remove('selected');
-        storage1tb.classList.remove('selected');
-    }
     storagePrice.innerText = '0';
     updateTotal()
 });
 storage512gb.addEventListener('click', function () {
-    if(!isContain(storage512gb)) {
-        storage256gb.classList.remove('selected');
-        storage512gb.classList.add('selected');
-        storage1tb.classList.remove('selected');
-    }
     storagePrice.innerText = '100';
     updateTotal()
 });
 storage1tb.addEventListener('click', function () {
-    if(!isContain(storage1tb)) {
-        storage256gb.classList.remove('selected');
-        storage512gb.classList.remove('selected');
-        storage1tb.classList.add('selected');
-    }
     storagePrice.innerText = '180';
     updateTotal()
 });
 
-// delivery
+// delivery price update
 freeDelivery.addEventListener('click', function () {
-    if(!isContain(freeDelivery)) {
-        freeDelivery.classList.add('selected');
-        paidDelivery.classList.remove('selected');
-    }
     deliveryCost.innerText = '0';
     updateTotal()
 });
 paidDelivery.addEventListener('click', function () {
-    if(!isContain(paidDelivery)) {
-        paidDelivery.classList.add('selected');
-        freeDelivery.classList.remove('selected');
-    }
     deliveryCost.innerText = '20';
     updateTotal()
 });
 
-// coupon
-couponButton.addEventListener('click', function () {
+// coupon code function
+function addCoupon() {
     updateTotal()
     code = couponCode.value;
     couponCode.value = '';
     if (code == 'stevekaku') {
         afterTotal = (total.innerText - (total.innerText * .20));
-        
         total.innerText = afterTotal;
         bottomTotal.innerText = afterTotal;
-        
     } else {
+        alert('False Coupon');
         return;
     }
-})
+};
 
-// price
+// total price function
 function updateTotal() {
     const bestCost = Number(bestPrice.innerText);
     const ramCost = Number(ramPrice.innerText);
